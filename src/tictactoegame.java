faccd26061a005509f0ef62ba26823d9dadf6266
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-15 21:20:52 -0700

    Added: Board Method

---
+++ src/tictactoegame.java
@@ -0,0 +1,19 @@
+
+
+public class tictactoegame {
+	
+
+	public static void Board()
+	{	
+		// Creating Board method
+		char[] Board = new char[10];
+		
+		//Assigning empty space for each value
+		for(int i = 0;i <= Board.length;i++)
+			Board[i]= ' ';
+		}
+//Calling Board function  
+ public static void main(String[]args) {
+	Board();
+ }
+ }
